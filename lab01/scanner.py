0c4dd21b859ab361fdc5a75f1dd86eaa92b8bd56
ubajzelj <uros@bajzelj.com>
2017-01-24 21:58:14 +0100

    adding port scanner

---
+++ lab01/scanner.py
@@ -0,0 +1,17 @@
+import telnetlib
+import socket
+
+# Port range
+low_port = 1
+high_port = 1000
+host = '192.168.35.121'
+
+# Iterate through all ports and try to establish the connection
+for port in range(low_port, high_port + 1):
+    try:
+        conn = telnetlib.Telnet(host, port)
+        conn.close()
+        print "PORT {}: OPEN".format(str(port))
+    # Catch the connections that are refused
+    except socket.error:
+        print "PORT {}: CLOSED".format(str(port))
